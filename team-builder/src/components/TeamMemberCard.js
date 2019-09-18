a618662ed1c8b33ff5bcd5f4386da6640ef083e3
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-09-18 15:20:29 -0700

    color changes

--- team-builder/src/components/TeamMemberCard.js
+++ team-builder/src/components/TeamMemberCard.js
@@ -5,7 +5,7 @@ const TeamMemberCard = props => {
     <div className ='card-list'>
       {props.people.map(card => (
         <div className ='card' key={card.id}>
-          <img src='https://images.unsplash.com/photo-1485001564903-56e6a54d46ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80' alt='randomImg'/>
+          <img src='https://images.unsplash.com/photo-1564889956728-9f3e4ad65498?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80' alt='randomImg'/>
           <p>Name: {card.name}</p>
           <p>Email: {card.email}</p>
           <p className ='role'>Role: {card.role}</p>
